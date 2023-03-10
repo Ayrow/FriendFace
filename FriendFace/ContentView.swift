56bd7a9ca662cb49dc678b4a2544aceb0d6d91f1
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-10 18:20:22 +0100

    Sorting users alphabetically in content view

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var cachedUsers: FetchedResults<CachedUser>
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var cachedUsers: FetchedResults<CachedUser>
     
     @State private var users = [User]()
     
@@ -32,28 +32,27 @@ struct ContentView: View {
                 .task {
                     if(cachedUsers.isEmpty){
                         await getUsers()
-                    }
-                    
-                    await MainActor.run {
-                        for user in users {
-                            let newUser = CachedUser(context: moc)
-                            newUser.name = user.name
-                            newUser.id = user.id
-                            newUser.isActive = user.isActive
-                            newUser.age = Int16(user.age)
-                            newUser.about = user.about
-                            newUser.email = user.email
-                            newUser.address = user.address
-                            newUser.company = user.company
-                            newUser.simplifiedDate = user.simplifiedDate
-                            
-                            for friend in user.friends {
-                                let newFriend = CachedFriend(context: moc)
-                                newFriend.id = friend.id
-                                newFriend.name = friend.name
-                                newFriend.user = newUser
+                        await MainActor.run {
+                            for user in users {
+                                let newUser = CachedUser(context: moc)
+                                newUser.name = user.name
+                                newUser.id = user.id
+                                newUser.isActive = user.isActive
+                                newUser.age = Int16(user.age)
+                                newUser.about = user.about
+                                newUser.email = user.email
+                                newUser.address = user.address
+                                newUser.company = user.company
+                                newUser.simplifiedDate = user.simplifiedDate
+                                
+                                for friend in user.friends {
+                                    let newFriend = CachedFriend(context: moc)
+                                    newFriend.id = friend.id
+                                    newFriend.name = friend.name
+                                    newFriend.user = newUser
+                                }
+                                try? moc.save()
                             }
-                            try? moc.save()
                         }
                     }
                     
