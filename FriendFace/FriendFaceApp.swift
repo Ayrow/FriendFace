4bc315b8a390b895023cdf2b1884d257c52510b9
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-10 13:24:48 +0100

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Aymeric Pilaert on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
