8892b7d3a33a99704004cfc3dde1793897160fb4
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-10 18:11:06 +0100

    added toolbar item to fast delete multiple users

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -15,47 +15,55 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            List(cachedUsers) { user in
-                HStack {
-                    NavigationLink {
-                        DetailView(user: user)
-                    } label: {
-                        Image(systemName: "person.fill")
-                            .foregroundColor(user.isActive ? .green : .red)
-                        Text(user.wrappedName)
+            List {
+                ForEach(cachedUsers) { user in
+                    HStack {
+                        NavigationLink {
+                            DetailView(user: user)
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .foregroundColor(user.isActive ? .green : .red)
+                            Text(user.wrappedName)
+                        }
                     }
+                }
+                .onDelete(perform: deleteUser)
+                .navigationTitle("FriendFace")
+                .task {
+                    if(cachedUsers.isEmpty){
+                        await getUsers()
+                    }
+                    
+                    await MainActor.run {
+                        for user in users {
+                            let newUser = CachedUser(context: moc)
+                            newUser.name = user.name
+                            newUser.id = user.id
+                            newUser.isActive = user.isActive
+                            newUser.age = Int16(user.age)
+                            newUser.about = user.about
+                            newUser.email = user.email
+                            newUser.address = user.address
+                            newUser.company = user.company
+                            newUser.simplifiedDate = user.simplifiedDate
+                            
+                            for friend in user.friends {
+                                let newFriend = CachedFriend(context: moc)
+                                newFriend.id = friend.id
+                                newFriend.name = friend.name
+                                newFriend.user = newUser
+                            }
+                            try? moc.save()
+                        }
+                    }
+                    
+            }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
                 }
             }
-            .navigationTitle("FriendFace")
-            .task {
-                if(cachedUsers.isEmpty){
-                    await getUsers()
-                }
-                
-                await MainActor.run {
-                    for user in users {
-                        let newUser = CachedUser(context: moc)
-                        newUser.name = user.name
-                        newUser.id = user.id
-                        newUser.isActive = user.isActive
-                        newUser.age = Int16(user.age)
-                        newUser.about = user.about
-                        newUser.email = user.email
-                        newUser.address = user.address
-                        newUser.company = user.company
-                        newUser.simplifiedDate = user.simplifiedDate
-                        
-                        for friend in user.friends {
-                            let newFriend = CachedFriend(context: moc)
-                            newFriend.id = friend.id
-                            newFriend.name = friend.name
-                            newFriend.user = newUser
-                        }
-                        try? moc.save()
-                    }
-                }
-                
-                }
             }
         }
     
@@ -85,6 +93,15 @@ struct ContentView: View {
         
     }
   
+    func deleteUser(at offsets: IndexSet) {
+        for offset in offsets {
+            let user = cachedUsers[offset]
+            
+            moc.delete(user)
+        }
+        try? moc.save()
+    }
+    
 }
     
 
